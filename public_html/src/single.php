3d5523ace30b9831656fb0988a5055cb2eb607cc
David <77265282+nikkeijin@users.noreply.github.com>
2023-11-13 12:36:23 +0900

    Update single.php

--- public_html/src/single.php
+++ public_html/src/single.php
@@ -8,6 +8,13 @@
 
         $the_title = $result->title;
         $the_content = $result->content;
+
+        if (isset($result->thumbnail) && isset($result->thumbnail->url)) {
+            $the_post_thumbnail_url = $result->thumbnail->url;
+        } else {
+            $the_post_thumbnail_url = 'https://picsum.photos/1024';
+        }
+        
     } else {
         header("Location: 404.php");
         exit;
