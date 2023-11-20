43d9d0466febaf7881e7a8ffaed2a85cacd27ac6
David <77265282+nikkeijin@users.noreply.github.com>
2023-11-20 15:27:50 +0900

    update

--- public_html/theme/layout/header.php
+++ public_html/theme/layout/header.php
@@ -3,13 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= get_template_directory_uri(); ?>/theme/assets/css/style.css">
+    <script defer src="<?= get_template_directory_uri(); ?>/theme/assets/js/app.js"></script>
     <title>
         <?php
             $title = 'Static';
-            if (is_front_page()) $title = 'Home';
-            if (is_page('contact')) $title = 'Contact Us!';
+            if (is_front_page())
+                $title = 'Home Page';
+            if (is_page('news'))
+                $title = 'News Archive';
+            if (is_page('post'))
+                $title = 'News Single';
+            if (is_page('contact'))
+                $title = 'Contact Us!';
             echo $title;
-        ?>
+            ?>
     </title>
 </head>
 <body>
